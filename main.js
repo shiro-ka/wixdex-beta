171979085e90b4cfa603133700ea84ffa6a4d3a7
shiroka. <167395925+shiro-ka@users.noreply.github.com>
2024-10-03 15:18:56 +0900

    js修正

--- main.js
+++ main.js
@@ -104,11 +104,9 @@ document.addEventListener('DOMContentLoaded', () => {
     searchInput.addEventListener('input', handleSearch);
 
     /* ルリグタイプ/クラス検索のポップアップ */
-    Array.from(document.getElementsByClassName('search-lrigTypeClass-button')).forEach(button => {
-        button.addEventListener('click', function() {
-            const lrigTypeClassPopup = document.getElementById('search-lrigTypeClass-popup');
-            lrigTypeClassPopup.style.display = lrigTypeClassPopup.style.display === 'none' ? 'block' : 'none'; // ボタンを押すとポップアップの表示・非表示を切り替え
-        });
+    document.getElementById('search-lrigTypeClass-popup').addEventListener('click', function() {
+        const lrigTypeClassPopup = document.getElementById('search-lrigTypeClass-popup');
+        lrigTypeClassPopup.style.display = lrigTypeClassPopup.style.display === 'none' ? 'block' : 'none';
     });
     /* ポップアップのボタン設定 */
     Array.from(document.getElementsByClassName('search-lrigTypeClass-button')).forEach(button => {
